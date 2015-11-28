985a29f8026b9570ca689ccac2d294661a158afa
mymtw <apple@mymtv.local>
2015-11-28 18:52:03 +0300

    fixed bug with response type of css files for chrome

--- webapp2_static.py
+++ webapp2_static.py
@@ -1,18 +1,28 @@
 import os
+import os.path
 import mimetypes
 import webapp2
 import logging
 
+
 class StaticFileHandler(webapp2.RequestHandler):
-    def get(self, path):
-        abs_path = os.path.abspath(os.path.join(self.app.config.get('webapp2_static.static_file_path', 'static'), path))
-        if os.path.isdir(abs_path) or abs_path.find(os.getcwd()) != 0:
-            self.response.set_status(403)
-            return
+    """A request handler for returning static files."""  
+    def get(self, asset_name):
+        """Serve out the contents of a file to self.response.
+        Args:
+        asset_name: The name of the static asset to serve. Must be in ASSETS_PATH.
+        """
+        abs_path = os.path.abspath(os.path.join(self.app.config.get('webapp2_static.static_file_path', 'static'), asset_name))
         try:
-            f = open(abs_path, 'r')
-            self.response.headers.add_header('Content-Type', mimetypes.guess_type(abs_path)[0])
-            self.response.out.write(f.read())
-            f.close()
-        except:
+            with open(abs_path, 'rb') as f:
+                data = f.read()
+        except (OSError, IOError):
+            logging.exception('Error reading file %s', abs_path)
             self.response.set_status(404)
+        else:
+            content_type, _ = mimetypes.guess_type(abs_path)
+
+            assert content_type, ('cannot determine content-type for %r' % abs_path)
+            self.response.headers['Content-Type'] = content_type
+            self.response.out.write(data)
+
